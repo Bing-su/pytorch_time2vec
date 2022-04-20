7b97b990bffc2409f0be14abc963a10408bc0abc
dowon <dowon@smartmind.team>
2022-04-20 18:00:32 +0900

    feat: first implements

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pytorch_time2vec
+
+### References
+
+[1] [Time2Vec for Time Series features encoding](https://towardsdatascience.com/time2vec-for-time-series-features-encoding-a03a4f3f937e)
+
+[2] [gdetor/pytorch_time2vec](https://github.com/gdetor/pytorch_time2vec)
---
+++ time2vec.py
@@ -0,0 +1,79 @@
+from math import sqrt
+from typing import Callable
+
+import torch
+from torch import nn
+
+
+class Time2Vec(nn.Module):
+    def __init__(
+        self,
+        in_features: int,
+        embedding_dim: int,
+        features_dim: int,
+        func: Callable[[torch.Tensor], torch.Tensor] = torch.sin,
+    ):
+        """
+        a Time2Vec implements class
+
+        Parameters
+        ----------
+        in_features : int
+            number of input features == sequence length
+        embedding_dim : int
+            number of output features per input column
+        features_dim : int
+            number of input columns
+        func : Callable[[Tensor], Tensor], optional
+            Torch periodic function (sin, cos, etc), by default torch.sin
+
+        Return
+        ------
+        torch.Tensor
+            shape of `(batch_size, in_features, (embedding_dim + 1) * features_dim)`
+        """
+        super().__init__()
+        self.in_features = in_features
+        self.embedding_dim = embedding_dim
+        self.features_dim = features_dim
+        self.func = func
+
+        embedding_out = embedding_dim * features_dim
+
+        self.W0 = nn.Parameter(torch.empty(in_features, in_features))
+        self.b0 = nn.Parameter(torch.empty(in_features, features_dim))
+        self.W = nn.Parameter(torch.empty(features_dim, embedding_out))
+        self.b = nn.Parameter(torch.empty(in_features, embedding_out))
+
+        for name in ("W0", "b0", "W", "b"):
+            self.register_parameter(name, getattr(self, name))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        "https://pytorch.org/docs/stable/_modules/torch/nn/modules/linear.html#Linear"
+        nn.init.kaiming_uniform_(self.W0, a=sqrt(5))
+        nn.init.kaiming_uniform_(self.W, a=sqrt(5))
+
+        fan_in0, _ = nn.init._calculate_fan_in_and_fan_out(self.W0)
+        bound0 = 1 / sqrt(fan_in0) if fan_in0 > 0 else 0
+        nn.init.uniform_(self.b0, -bound0, bound0)
+        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.W)
+        bound = 1 / sqrt(fan_in) if fan_in > 0 else 0
+        nn.init.uniform_(self.b, -bound, bound)
+
+    def forward(self, tau: torch.Tensor) -> torch.Tensor:
+        # tau : (batch_size, in_features, features_dim)
+        batch_size = tau.shape[0]
+
+        W0 = self.W0.repeat(batch_size, 1, 1)
+        b0 = self.b0.repeat(batch_size, 1, 1)
+        W = self.W.repeat(batch_size, 1, 1)
+        b = self.b.repeat(batch_size, 1, 1)
+
+        original = torch.bmm(W0, tau) + b0
+        sin_trans = self.func(torch.bmm(tau, W) + b)
+
+        # output : (batch_size, in_features, (embedding_dim + 1) * features_dim)
+        output = torch.cat([sin_trans, original], dim=-1)
+        return output
